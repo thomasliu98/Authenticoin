8b3d5284748b602596b191dff3633bc0fa983b26
sawyercg <csawyer1738@gmail.com>
2020-03-09 09:51:00 -0500

    Added Data and minting permissions

---
+++ contracts/AuthenticoinBase.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.5.16;
+import "./ContractERC721.sol"
+import "./SafeMath.sol"
+import "./AddressUtils.sol"
+import "./AuthenticoinMintcontrol"
+contract AuthenticoinBase is ContractERC721, AuthenticoinMintcontrol{
+    using SafeMath for uint256;
+    using AddressUtils for address;
+    struct Authenticoin{
+        uint256 companyID;
+        uint256 productID;
+        uint256 productMetadata;
+    }
+
+    /**
+    *@dev Array containing all coins. 
+    *The coinID of each coin will map to the arrayIndex of the coin
+     */
+    Authenticoin [] coins;
+
+    function _mint(
+    address _to,
+    uint256 _companyID,
+    uint256 _productID,
+    uint256 _productMetadata
+  ) 
+    internal
+    onlyCreator
+    returns (uint){
+        Authenticoin memory _coin = Authenticoin({
+            companyID: _companyID,
+            productID: _productID,
+            productMetadata: _productMetadata
+        })
+        uint256 newCoinID= coins.push(_coin) - 1;
+        _transfer(_to, newCoinID);
+        return newCoinID;
+  }
+}
---
+++ contracts/AuthenticoinMintControl.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.16;
+import "./SafeMath.sol"
+import "./AddressUtils.sol"
+
+contract AuthenticoinMintControl{
+    using SafeMath for uint256;
+    using AddressUtils for address;
+
+    address public creatorAddress;
+
+
+    modifier onlyCreator(){
+        require(msg.sender == creatorAddress);
+    }
+
+}
